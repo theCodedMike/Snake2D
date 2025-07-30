09f79e0430d450401a60bbda64c9335e3b58f8cf
lixia <lixiaofeng1115@outlook.com>
2025-07-30 15:59:27 +0800

    生成食物

---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnFood : MonoBehaviour
+{
+    [Header("食物")]
+    public GameObject foodPrefab;
+    [Header("左边界")]
+    public Transform borderLeft;
+    [Header("右边界")]
+    public Transform borderRight;
+    [Header("上边界")]
+    public Transform borderTop;
+    [Header("下边界")]
+    public Transform borderBottom;
+
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        int x = Random.Range((int)borderLeft.position.x + 1, (int)borderRight.position.x - 1);
+        int y = Random.Range((int)borderBottom.position.y + 1, (int)borderTop.position.y - 1);
+        print($"Food position: {y}, {x}");
+        Instantiate(foodPrefab, new Vector3(x, y, 0), Quaternion.identity);
+    }
+}
